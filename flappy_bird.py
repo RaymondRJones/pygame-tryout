f97f7ddf57201a1985039e985a6562b7e2dff3f0
RaymondJones <Rayjones2170@gmail.com>
2023-10-04 19:13:43 -0400

    Adds sprites and core functions

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,9 +4,12 @@ import random
 # Initialize pygame
 pygame.init()
 
+
+
 # Constants
-WIDTH = 480
-HEIGHT = 640
+WIDTH = 288
+HEIGHT = 512
+
 BIRD_WIDTH = 50
 BIRD_HEIGHT = 50
 PIPE_WIDTH = 70
@@ -21,10 +24,24 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
+background_image = pygame.image.load('game_objects/background-day.png')
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+screen.blit(background_image, (0, 0))
+
+
 bird_y = HEIGHT // 2
 bird_y_change = 0
 
 pipes = []
+
+bird_image = pygame.image.load('game_objects/yellowbird-midflap.png')
+bird_image = pygame.transform.scale(bird_image, (BIRD_WIDTH, BIRD_HEIGHT))
+
+# pipe_top_image = pygame.image.load('pipe_top.png')
+pipe_bottom_image = pygame.image.load('game_objects/pipe-green.png')
+pipe_top_flipped_image = pygame.transform.flip(pipe_bottom_image, False, True)
+
+
 
 def create_pipe():
     pipe_height = random.randint(100, 400)
@@ -36,8 +53,10 @@ def move_pipes():
 
 def draw_pipes():
     for pipe in pipes:
-        pygame.draw.rect(screen, GREEN, (pipe['x'], 0, PIPE_WIDTH, pipe['top']))
-        pygame.draw.rect(screen, GREEN, (pipe['x'], pipe['bottom'], PIPE_WIDTH, HEIGHT))
+        # pygame.draw.rect(screen, GREEN, (pipe['x'], 0, PIPE_WIDTH, pipe['top']))
+        screen.blit(pipe_top_flipped_image, (pipe['x'], pipe['top'] - pipe_top_flipped_image.get_height()))
+        screen.blit(pipe_bottom_image, (pipe['x'], pipe['bottom']))
+        # pygame.draw.rect(screen, GREEN, (pipe['x'], pipe['bottom'], PIPE_WIDTH, HEIGHT))
 
 def check_collision(bird_y):
     for pipe in pipes:
@@ -50,23 +69,23 @@ def check_collision(bird_y):
 
 
 create_pipe()
-
+# game_paused = False
 # Game loop
 running = True
 while running:
-    screen.fill(WHITE)
-    
+    # screen.fill(WHITE)
+    screen.blit(background_image, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bird_y_change = -5
+                bird_y_change = -10
                 
     bird_y_change += 0.5
     bird_y += bird_y_change
     
-    pygame.draw.rect(screen, GREEN, (WIDTH // 2, bird_y, BIRD_WIDTH, BIRD_HEIGHT))
+    screen.blit(bird_image, (WIDTH // 2, bird_y))
     move_pipes()
     draw_pipes()
 
