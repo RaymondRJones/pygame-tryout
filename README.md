776d9c9f065ad71c4fb59576d070891ba5d7fd49
RaymondJones <Rayjones2170@gmail.com>
2023-10-05 11:29:00 -0400

    clean up the code a bit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pygame-tryout
+# Flappy Bird in pygame
+
+# Running the Game
+This is all python and pygame code so you can run it with
+
+`pip install pygame`
+`python flappy_bird.py`
+
+# Summary
+
+I decided to randomly try out pygame and see how it's functions and logic works. What better way than to recreate an popular game from like 2015.
--- flappy_bird.py
+++ flappy_bird.py
@@ -1,10 +1,9 @@
 import pygame
 import random
+import time
 
 # Initialize pygame
 pygame.init()
-
-
 
 # Constants
 WIDTH = 288
@@ -24,10 +23,10 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
+# Load / initialize the images
 background_image = pygame.image.load('game_objects/background-day.png')
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 screen.blit(background_image, (0, 0))
-
 
 bird_y = HEIGHT // 2
 bird_y_change = 0
@@ -37,12 +36,17 @@ pipes = []
 bird_image = pygame.image.load('game_objects/yellowbird-midflap.png')
 bird_image = pygame.transform.scale(bird_image, (BIRD_WIDTH, BIRD_HEIGHT))
 
-# pipe_top_image = pygame.image.load('pipe_top.png')
+bird_down_image = pygame.image.load('game_objects/yellowbird-downflap.png')
+bird_down_image = pygame.transform.scale(bird_image, (BIRD_WIDTH, BIRD_HEIGHT))
+
+bird_up_image = pygame.image.load('game_objects/yellowbird-upflap.png')
+bird_up_image = pygame.transform.scale(bird_image, (BIRD_WIDTH, BIRD_HEIGHT))
+
 pipe_bottom_image = pygame.image.load('game_objects/pipe-green.png')
 pipe_top_flipped_image = pygame.transform.flip(pipe_bottom_image, False, True)
 
 
-
+# functions
 def create_pipe():
     pipe_height = random.randint(100, 400)
     pipes.append({'x': WIDTH, 'top': pipe_height - PIPE_WIDTH, 'bottom': pipe_height + PIPE_GAP})
@@ -53,10 +57,8 @@ def move_pipes():
 
 def draw_pipes():
     for pipe in pipes:
-        # pygame.draw.rect(screen, GREEN, (pipe['x'], 0, PIPE_WIDTH, pipe['top']))
         screen.blit(pipe_top_flipped_image, (pipe['x'], pipe['top'] - pipe_top_flipped_image.get_height()))
         screen.blit(pipe_bottom_image, (pipe['x'], pipe['bottom']))
-        # pygame.draw.rect(screen, GREEN, (pipe['x'], pipe['bottom'], PIPE_WIDTH, HEIGHT))
 
 def check_collision(bird_y):
     for pipe in pipes:
@@ -69,23 +71,21 @@ def check_collision(bird_y):
 
 
 create_pipe()
-# game_paused = False
 # Game loop
 running = True
+time.sleep(2) # Delay to the game before starting it on the screen
 while running:
-    # screen.fill(WHITE)
     screen.blit(background_image, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird_y_change = -10
-                
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            bird_y_change = -10
     bird_y_change += 0.5
     bird_y += bird_y_change
     
     screen.blit(bird_image, (WIDTH // 2, bird_y))
+    
     move_pipes()
     draw_pipes()
 
